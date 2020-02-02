ec0b14712396d9df5f99dd6b9bf66729b1348987
Danial <danial.kordmodanlou@gmail.com>
2020-02-02 21:11:53 +0330

    now it connects very well using this script

--- TwitchDroneRemote/Assets/NetworkHandler.cs
+++ TwitchDroneRemote/Assets/NetworkHandler.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
 
-public class NetworkHandler : MonoBehaviour
+
+public class NetworkHandler :  MonoBehaviour {
+void Start() {
+    
+}
+
+
+private void Update()
 {
-    // Start is called before the first frame update
-    void Start()
+    if (Input.GetKeyDown(KeyCode.P))
     {
-        
+        print("cliked");
+        StartCoroutine(Upload());
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+IEnumerator Upload() {
+    print("sent");
+    WWWForm form = new WWWForm();
+    form.AddField("State", "Hi its Dan");
+ 
+    UnityWebRequest www = UnityWebRequest.Post("192.168.4.1", form);
+    print(www.SendWebRequest().ToString());
+    yield return www.SendWebRequest();
+ 
+    if(www.isNetworkError || www.isHttpError) {
+        Debug.Log(www.error);
     }
+    else {
+        Debug.Log("Form upload complete!");
+    }
+}
 }
